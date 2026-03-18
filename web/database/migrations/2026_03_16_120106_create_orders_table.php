75ca4a652eda0eee87f103a0d9f542f78fca6da5
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-18 23:14:09 +0000

    feat(migration): update orders and order_items tables with status and product_id fields

--- web/database/migrations/2026_03_16_120106_create_orders_table.php
+++ web/database/migrations/2026_03_16_120106_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->enum('status', ['en attente', 'en preparation', 'livrée']);
+            $table->enum('status', ['pending', 'preparing', 'delivered', 'cancelled']);
             $table->timestamps();
         });
     }
