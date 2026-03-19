9193e2baecd44903cb60d9620a661ec16cafca83
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-19 00:12:48 +0000

    fix(migration): make ItemPrice nullable in order_items table

--- web/database/migrations/2026_03_16_120344_create_order_items_table.php
+++ web/database/migrations/2026_03_16_120344_create_order_items_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('order_id')->constrained('orders');
             $table->foreignId('product_id')->constrained('products');
             $table->integer('quantity');
-            $table->decimal('ItemPrice');
+            $table->decimal('ItemPrice')->nullable();
             $table->timestamps();
         });
     }
