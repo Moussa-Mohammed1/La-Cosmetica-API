b9722f24e4e45c08897742471be208d5e13b603e
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-19 00:11:44 +0000

    feat(provider): bind DAO interfaces to their implementations in AppServiceProvider

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\DAO\CategoryDAO;
+use App\DAO\Interfaces\CategoryDAOInterface;
+use App\DAO\Interfaces\OrderDAOInterface;
+use App\DAO\Interfaces\ProductDAOInterface;
+use App\DAO\OrderDAO;
+use App\DAO\ProductDAO;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryDAOInterface::class, CategoryDAO::class);
+        $this->app->bind(ProductDAOInterface::class, ProductDAO::class);
+        $this->app->bind(OrderDAOInterface::class, OrderDAO::class);
     }
 
     /**
